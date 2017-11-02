229aadd6cd889bc7527bf618e0d74e2cb603240a
Brandon R <games@open-mailbox.com>
2017-11-01 23:20:18 -0400

    Fetch stats for item details page.

--- scripts/details.js
+++ scripts/details.js
@@ -1,5 +1,5 @@
 var API_KEY = "7435cdf7f5a44aa6a110117181d76c6b";
-var URL_ROOT = "https://www.bungie.net/Platform/Destiny2/Manifest/DestinyInventoryItemDefinition/";
+var URL_ROOT = "https://www.bungie.net/Platform/Destiny2/Manifest/";
 
 var app = new Vue({
   el: "#app",
@@ -8,6 +8,7 @@ var app = new Vue({
     params: new URLSearchParams(window.location.search.substr(1)),
     itemId: null,
     item: { displayProperties: {} },
+    stats: []
   },
 
   methods: {
@@ -17,14 +18,62 @@ var app = new Vue({
       }
     },
 
+    getStat: function(hash) {
+      var stat = null;
+
+      for (var i = 0; i < app.stats.length; i++) {
+        if (app.stats[i].hash === hash) {
+          stat = {hash: hash, name: app.stats[i].displayProperties.name};
+          break;
+        }
+      }
+
+      for (var i = 0; i < app.item.investmentStats.length; i++) {
+        if (app.item.investmentStats[i].statTypeHash === hash) {
+          stat.value = app.item.investmentStats[i].value;
+          break;
+        }
+      }
+
+      return stat;
+    },
+
     goBack: function () {
       window.history.back();
+    },
+
+    fetchStats: function() {
+      for (var i = 0; i < app.item.investmentStats.length; i++) {
+        if (app.item.investmentStats[i].value === 0) { continue; }
+
+        var hash     = app.item.investmentStats[i].statTypeHash;
+        var url      = URL_ROOT + "DestinyStatDefinition/" + hash + "/";
+        var request  = new XMLHttpRequest();
+
+        request.open("GET", url, true);
+
+        request.setRequestHeader("X-API-KEY", API_KEY);
+
+        request.onload = function(event) {
+          if (event.currentTarget.status >= 200 && event.currentTarget.status < 400) {
+            var response = JSON.parse(event.currentTarget.responseText);
+            app.stats.push(response.Response);
+          }
+        };
+
+        request.onerror = function() {
+          console.error("Error whilemaking ajax request.");
+          console.error(request);
+        }
+
+        request.send();
+      }
     }
   },
 
   created: function () {
     this.itemId = this.params.get("itemId");
-    var url     = URL_ROOT + this.itemId + "/";
+    var url     = URL_ROOT + "DestinyInventoryItemDefinition/" + this.itemId + "/";
     var request = new XMLHttpRequest();
     var app     = this;
 
@@ -36,13 +85,13 @@ var app = new Vue({
       if (request.status >= 200 && request.status < 400) {
         var response = JSON.parse(request.responseText);
         app.item = response.Response;
-        console.log(app.item);
+        app.fetchStats();
       }
     };
 
     request.onerror = function () {
       console.error("Error while making ajax request.");
-      console.error("request");
+      console.error(request);
     };
 
     request.send();
